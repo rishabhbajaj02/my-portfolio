05c0c29ead0db7e9711cb017838736e1c88f6c67
Rishabh Bajaj <bajajris@shernet.sheridancollege.ca>
2022-01-28 16:04:01 -0500

    text changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,9 +11,7 @@ const Projects = () => {
         Apps I've Built
       </h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-black">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-        facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-        fuga dolore.
+        Here are some of the projects I built
       </p>
     </div>
     <div className="flex flex-wrap -m-4">
